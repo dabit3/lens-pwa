3667bf504eb7315d8dae2b22599bcfc71975c4c5
bijan <bmassoumi@gmail.com>
2023-09-24 00:31:16 -0400

    update

--- app/hooks/handleBuyToken.ts
+++ app/hooks/handleBuyToken.ts
@@ -8,34 +8,30 @@ const useHandleBuyToken = (contract: string, amount: number) => {
     "idle" | "pending" | "success" | "error"
   >("idle");
 
-  useEffect(() => {
-    const embeddedWallet = wallets.find(
-      (wallet) => wallet.walletClientType === "privy"
-    );
-    if (!embeddedWallet) {
+  const embeddedWallet = wallets.find(
+    (wallet) => wallet.walletClientType === "privy"
+  );
+  if (!embeddedWallet) {
+    setStatus("error");
+    throw new Error("embedded wallet empty");
+  }
+
+  const trigger = async () => {
+    try {
+      setStatus("pending");
+      embeddedWallet.switchChain(1337);
+      const provider = await embeddedWallet.getEthersProvider(); // ethers provider object
+      const signer = provider.getSigner(); // ethers signer object
+      const fac = ShareSample__factory.connect(contract, signer);
+      await fac.buyShares(amount);
+      setStatus("success");
+    } catch (error) {
+      console.error("Error buying token:", error);
       setStatus("error");
-      throw new Error("embedded wallet empty");
     }
+  };
 
-    const run = async () => {
-      try {
-        setStatus("pending");
-        embeddedWallet.switchChain(1337);
-        const provider = await embeddedWallet.getEthersProvider(); // ethers provider object
-        const signer = provider.getSigner(); // ethers signer object
-        const fac = ShareSample__factory.connect(contract, signer);
-        await fac.buyShares(amount);
-        setStatus("success");
-      } catch (error) {
-        console.error("Error buying token:", error);
-        setStatus("error");
-      }
-    };
-
-    run();
-  }, [contract, amount, wallets]);
-
-  return status;
+  return { trigger, status };
 };
 
 export default useHandleBuyToken;
