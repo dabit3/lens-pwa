e9c7a221157aecc2380b820e00ef2fa49451b1c0
bijan <bmassoumi@gmail.com>
2023-09-24 06:06:28 -0400

    gen users

--- scripts/generateUsers.ts
+++ scripts/generateUsers.ts
@@ -6,6 +6,7 @@ const addresses = [
   "0x70997970C51812dc3A010C7d01b50e0d17dc79C8",
   "0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC",
   "0xbDF88a5969aE589286c4504a21c83EEe37C82164",
+  "0x23618e81E3f5cdF7f54C3d65f7FBc0aBf5B21E8f",
 ];
 
 async function run() {
@@ -13,9 +14,10 @@ async function run() {
   for (const address of addresses) {
     try {
       const tx = await factory.createShareSample(address);
-      const res = await tx.wait();
+      await tx.wait();
     } catch (error) {
       console.log(error);
+      continue;
     }
 
     const [addresses, contracts] = await factory.getDeployedContracts();
