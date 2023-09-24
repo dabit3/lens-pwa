82422648df44724d76ada004be5425193f1daef7
bijan <bmassoumi@gmail.com>
2023-09-23 21:44:23 -0400

    commnet out users post

---
+++ app/api/transact/route.ts
@@ -0,0 +1,2 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -17,32 +17,32 @@ export async function GET(request: NextRequest) {
   );
 }
 
-export async function POST(request: NextRequest) {
-  const data = await request.text();
+// export async function POST(request: NextRequest) {
+//   const data = await request.text();
 
-  let parsedBody;
-  try {
-    parsedBody = JSON.parse(data);
-  } catch (error) {
-    return NextResponse.json(
-      { message: "Invalid JSON in request body." },
-      { status: 400 }
-    );
-  }
+//   let parsedBody;
+//   try {
+//     parsedBody = JSON.parse(data);
+//   } catch (error) {
+//     return NextResponse.json(
+//       { message: "Invalid JSON in request body." },
+//       { status: 400 }
+//     );
+//   }
 
-  if (!parsedBody.walletAddress || !parsedBody.contractAddress) {
-    return NextResponse.json(
-      { message: "Missing walletAddress or contractAddress." },
-      { status: 400 }
-    );
-  }
+//   if (!parsedBody.walletAddress || !parsedBody.contractAddress) {
+//     return NextResponse.json(
+//       { message: "Missing walletAddress or contractAddress." },
+//       { status: 400 }
+//     );
+//   }
 
-  const newUser: User = {
-    id: Date.now(),
-    walletAddress: parsedBody.walletAddress,
-    contractAddress: parsedBody.contractAddress,
-  };
+//   const newUser: User = {
+//     id: Date.now(),
+//     walletAddress: parsedBody.walletAddress,
+//     contractAddress: parsedBody.contractAddress,
+//   };
 
-  users.push(newUser);
-  return NextResponse.json(newUser, { status: 201 });
-}
+//   users.push(newUser);
+//   return NextResponse.json(newUser, { status: 201 });
+// }
